c84cd199c8b9ad8fb86a6611dcfbc3fa20cba237
Ramabotz9 <108044415+Ramabotz9@users.noreply.github.com>
2022-07-08 15:27:20 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
-let text = `*${htki} MAU NGAPAIN OM ${htka}*
+let text = `*${htki} SC REACORD OM CMN PVT ${htka}*
 `
 const templateButtons = [
     {index: 1, urlButton: {displayText: '🐈 My Github', url: sgh}},
